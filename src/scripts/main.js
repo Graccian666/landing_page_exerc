1d593a169fccbdd34ffeb61c2d2fe62ff91e6f43
Rafael Graciano Ribeiro <rafagraciribeiro@gmail.com>
2024-06-28 18:17:59 -0300

    correção e adição do faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreFecha)   
+    }
+
+
+
+
     window.addEventListener('scroll', function(){
 
         const scrollAtual = window.scrollY
@@ -13,17 +22,24 @@ document.addEventListener('DOMContentLoaded', function(){
 
     })
 
-
-    function show(){
-        const personagens = document.querySelector('[data-personagem]')
-
-        personagens.classList.add('personagens__content__list--is-visible')
-    }
-
-    function hide(){
-        const personagens = document.querySelector('[data-personagem]')
-
-        personagens.classList.remove('personagens__content__list--is-visible')
-    }
-
 })
+
+function show(){
+    const personagens = document.querySelector('[data-personagem]')
+
+    personagens.classList.add('personagens__content__list--is-visible')
+}
+
+function hide(){
+    const personagens = document.querySelector('[data-personagem]')
+
+    personagens.classList.remove('personagens__content__list--is-visible')
+}
+
+function abreFecha(elemento) {
+    const classe = 'faq__list__item--is-open';  
+
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
